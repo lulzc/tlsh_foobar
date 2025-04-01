5cc37b474a2f660d0f8d81632b5765eca2370828
lolcat <139807587+lulzc@users.noreply.github.com>
2025-04-01 15:00:02 +0200

    Create main.go

---
+++ server/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"tlsh_foobar/server/internal/tlsh_foo"
+	"tlsh_foobar/server/internal/transport"
+)
+
+func main() {
+
+	var csvFile string
+
+	flag.StringVar(&csvFile, "csv", "", "path to CSV file")
+
+	flag.Usage = func() {
+		log.Println("Usage: ./tlshServer -csv csvFile ")
+	}
+	flag.Parse()
+
+	if csvFile == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	svc := tlsh_foo.NewService()
+	server := transport.NewServer(svc)
+
+	//var csvFile = "./server/internal/db/sqlite.db"
+	svc.ReadCsv(csvFile)
+
+	if err := server.Serve(); err != nil {
+		log.Println(err)
+	}
+
+}
